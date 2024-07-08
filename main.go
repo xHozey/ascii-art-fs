975b003623a2da01677c02d54bf144db6554a4d1
xHozey <hamzasaliheddine@gmail.com>
2024-07-08 10:18:09 +0100

    hh

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
+	"net/http"
+
 	ft "web/features"
 )
 
+func serveStatic(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	t.Execute(w, r)
+}
+
 func main() {
 	ft.AsciiArt()
-
+	http.HandleFunc("/", serveStatic)
+	http.ListenAndServe(":8080", nil)
 }
